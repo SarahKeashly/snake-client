6b7a9185b0b5777ea58c4763b7f6da8999f21711
Sarah <sarahejohnson89@gmail.com>
2021-09-22 17:49:21 -0700

    moved up and name added

---
+++ client.js
@@ -0,0 +1,43 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: '135.23.223.133',
+    port: 50542,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("you are successfully connected to the game server");
+    // code that does something when the connection is first established
+
+  });
+
+  conn.on("connect", () => {
+    conn.write('Name: SEK');
+
+  });
+
+  conn.on("connect", () => {
+    conn.write('Move: up');
+  });
+
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+
+
+module.exports = {
+
+  connect,
+
+}
+
+
